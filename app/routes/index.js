30860cacc9a50ff6748b6e43c650aa166c44037e
Paul Vilchez <vilchez.paul@gmail.com>
2015-06-13 23:26:26 -0400

    Added IBM Sentiment Analysis in main route.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
+var https = require('https');
 var router = express.Router();
+var config = require('../config.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BinFiddle' });
+
+  var headers = {
+    'User-Agent': 'Super Agent/0.0.1',
+    'Content-Type': 'application/x-www-form-urlencoded'
+  };
+  var text = "this is awesome!"
+  var getAnalyzeSentimentOpt = function(text) {
+    return {
+      host: config.hpHostUrl,
+      port: 443,
+      path: '/1/api/sync/analyzesentiment/v1?text=' +
+        encodeURIComponent(text).replace(/%20/g,'+') +
+        '&apikey=' + config.hpApiKey,
+      headers: headers
+    };
+  };
+  var sent = https.get(getAnalyzeSentimentOpt(text), function(response) {
+    var str = '';
+    response.on('data', function(chunk) {
+      str += chunk;
+    });
+    response.on('end', function() {
+      var json = JSON.parse(str);
+      global_sentiment = json;
+      console.log(global_sentiment);
+      res.render('index', { title: config.hostUrl });
+    });
+  });
 });
 
 module.exports = router;
